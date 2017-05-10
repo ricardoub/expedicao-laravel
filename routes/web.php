4a005a7b44988a18ffce89eacdb90a0ec2509afd
Ricardo Bomfim <ricardoub@gmail.com>
2017-05-10 16:33:48 -0300

    tarefas: routes to tarefas included

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'tarefas', 'as' => 'tarefas.'], function() {
+  Route::get('',             ['as' => 'index',   'uses' => 'TarefaController@index']);
+  Route::get('create',       ['as' => 'create',  'uses' => 'TarefaController@create']);
+  Route::post('store',       ['as' => 'store',   'uses' => 'TarefaController@store']);
+  Route::get('show/{id}',    ['as' => 'show',    'uses' => 'TarefaController@show']);
+  Route::get('edit/{id}',    ['as' => 'edit',    'uses' => 'TarefaController@edit']);
+  Route::post('update/{id}', ['as' => 'update',  'uses' => 'TarefaController@update']);
+  Route::get('delete/{id}',  ['as' => 'delete',  'uses' => 'TarefaController@delete']);
+  Route::post('destroy/{id}',['as' => 'destroy', 'uses' => 'TarefaController@destroy']);
+});
