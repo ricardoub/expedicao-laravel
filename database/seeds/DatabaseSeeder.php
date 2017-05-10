2cabb393bbce4ffb7cbd7177c930888df1045af8
Ricardo Bomfim <ricardoub@gmail.com>
2017-05-10 20:31:57 -0300

    combos - model, migration and seeder included

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
    */
   public function run()
   {
+    $this->call(CombosTableSeeder::class);
+
     $this->call(PermissionsTableSeederUsers::class);
     $this->call(PermissionsTableSeederTasks::class);
     $this->call(RolesTableSeeder::class);
