09833e033adc0cbff9c6bdbb91282de69f6f1ff6
Ricardo Bomfim <ricardoub@gmail.com>
2017-05-10 20:32:46 -0300

    partials - task menu item and button home included

--- resources/views/partials/buttons/panelButton-home.blade.php
+++ resources/views/partials/buttons/panelButton-home.blade.php
@@ -1,4 +1,7 @@
-<a href="{{ url('home') }}" class="btn btn-default" >
-  <i class="fa fa-home fa-fw"></i>
-  <span class="hidden-xs hidden-sm">Inicio</span>
+<a  href="{{ route( $buttons['home']['link'] ) }}"
+    class="btn btn-{{ $buttons['home']['class'] }}" >
+  <i class="fa fa-fw fa-{{ $buttons['home']['icon'] }}"></i>
+  <span class="hidden-xs hidden-sm">
+    {{ $buttons['home']['name'] }}
+  </span>
 </a>
