d1fbc6458cb4ec20d1204ff6fe203ad1ce56e1bd
Ricardo Bomfim <ricardoub@gmail.com>
2017-05-12 18:32:02 -0300

    tasks: views created

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.bootstrap.panel')
+
+@section('panel-head-middle')
+  <button type="button" class="btn btn-default btn-title form-control" disabled>
+    <i class="fa fa-tasks"></i>
+    Exibir
+    <span class="hidden-xs hidden-sm">Tarefa</span>
+  </button>
+@endsection
+
+@section('panel-head-left')
+  @include('partials.buttons.link', [
+    'btnName'  => $buttons['back']['name'],
+    'btnLink'  => $buttons['back']['link'],
+    'btnIcon'  => $buttons['back']['icon'],
+    'btnClass' => $buttons['back']['class'],
+  ])
+@endsection
+
+@section('panel-head-right')
+  @include('partials.buttons.linkWithId', [
+    'btnName'  => $buttons['edit']['name'],
+    'btnLink'  => $buttons['edit']['link'],
+    'btnIcon'  => $buttons['edit']['icon'],
+    'btnClass' => $buttons['edit']['class'],
+  ])
+  @include('partials.buttons.linkWithId', [
+    'btnName'  => $buttons['delete']['name'],
+    'btnLink'  => $buttons['delete']['link'],
+    'btnIcon'  => $buttons['delete']['icon'],
+    'btnClass' => $buttons['delete']['class'],
+  ])
+@endsection
+
+@section('panel-body')
+  <br>
+  <div class="">
+    <div class="form-group col-md-8 col-md-offset-2">
+      @include('partials.messages')
+      @include('tasks.form')
+    </div>
+  </div>
+@endsection
+
+@section('page-scripts')
+  <!--<script src="/js/todos.js"></script>-->
+@stop
